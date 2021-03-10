5b482d0d914ae96f1b59bbc4d8d7f366a6070d72
Cody Snell <codyjsnell@yahoo.com>
2021-03-10 14:45:20 -0600

    adds toggle class function

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-alert('home')
+const open = document.getElementById('open')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () => container.classList.toggle('show-nav'))
